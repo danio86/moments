f525e54c4f2aae1a76df6419239abfc5250c3445
danio86 <d.harder@gmx.at>
2023-05-28 15:23:04 +0000

    add react router and nav links

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import styles from "./App.module.css";
 import NavBar from "./components/NavBar";
+import Container from "react-bootstrap/Container";
+import { Route, Switch } from "react-router-dom";
+/* Bevor wir über das Routing sprechen, importieren wir einen Container von Bootstrap, 
+um unseren gesamten Hauptinhalt einzuschließen. 
+In der Datei App.js importieren wir den Container mit 
+"react-bootstrap/Container". Anschließend fügen wir unsere 
+Container-Komponente unterhalb der Navbar hinzu. */
+
+/* der router lässt inhalte je nach url anzeigen (wie if path) */
+/* muss in index.js */
 
 function App() {
   return (
     <div className={styles.App}>
       {/* styles wurde imported, App ist der Classname in App.module.css */}
       <NavBar />
+      <Container className={styles.Main}>
+        <Switch>
+          <Route exact path="/" render={() => <h1>Home page</h1>} />
+          {/* wenn der route path matched, wird h1 gerendert */}
+          <Route exact path="/signin" render={() => <h1>Sign in</h1>} />
+          <Route exact path="/signup" render={() => <h1>Sign up</h1>} />
+          <Route render={() => <p>Page not found!</p>} />
+        </Switch>
+      </Container>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter as Router } from "react-router-dom";
+/* der router lässt inhalte je nach url anzeigen (wie if path) */
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+      {/* die App muss in den Router */}
+    </Router>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
