584238d60fbc83b747778860edde0d78c4faf3aa
danio86 <>
2023-05-30 14:54:58 +0000

    add createContext hook

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import styles from "./App.module.css";
 import NavBar from "./components/NavBar";
 import Container from "react-bootstrap/Container";
+import { Route, Switch } from "react-router-dom";
 import "./api/axiosDefaults";
 import SignUpForm from "./pages/auth/SignUpForm";
 import SignInForm from "./pages/auth/SignInForm";
-import { Route, Switch } from "react-router-dom";
+import { createContext, useEffect, useState } from "react";
+import axios from "axios";
 
 /* Bevor wir über das Routing sprechen, importieren wir einen Container von Bootstrap, 
 um unseren gesamten Hauptinhalt einzuschließen. 
@@ -15,21 +17,60 @@ Container-Komponente unterhalb der Navbar hinzu. */
 /* der router lässt inhalte je nach url anzeigen (wie if path) */
 /* muss in index.js */
 
+export const CurrentUserContext = createContext();
+export const SetCurrentUserContext = createContext();
+/* So, in order to make it more accessible,  we’ll use the useContext hook. The React  
+documentation states that context provides  a convenient way to pass data required by  
+many components in an application. Essentially,  Context is designed to share data that can be  
+considered “global” to any child components that  need access to it */
+
+/* jedes mal wenn die se Funktionen aufgerufen werden, 
+wir ein neues Context Objekt kreiert */
+
 function App() {
+
+    const [currentUser, setCurrentUser] = useState(null);
+    /* Hook mit Wert Null (User ist nicht eingelogged) */
+
+    const handleMount = async () => {
+      try {
+        const { data } = await axios.get("dj-rest-auth/user/");
+        setCurrentUser(data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+  
+    useEffect(() => {
+      handleMount();
+    }, []);
+    /* wir checken hier EINMAL ob ein User eingelogged ist */
+
+
   return (
-    <div className={styles.App}>
-      {/* styles wurde imported, App ist der Classname in App.module.css */}
-      <NavBar />
-      <Container className={styles.Main}>
-        <Switch>
-          <Route exact path="/" render={() => <h1>Home page</h1>} />
-          {/* wenn der route path matched, wird h1 gerendert */}
-          <Route exact path="/signin" render={() => <SignInForm />} />
-          <Route exact path="/signup" render={() => <SignUpForm />} />
-          <Route render={() => <p>Page not found!</p>} />
-        </Switch>
-      </Container>
-    </div>
+    <CurrentUserContext.Provider value={currentUser}>
+        {/* jedes Context-Objekt hat einen Provider, 
+        der den aktuellen Wert/Zustand beschreibt. In diesem fall
+        wird der uste state Hook beschrieben.
+        Dieser Zustand ist nun in jedem Child-Component, also alles im 
+        folgendem DIV verfügbar! Hier muss der Zusatnd allerdings abgefragt
+        werden */}
+      <SetCurrentUserContext.Provider value={setCurrentUser}>
+        <div className={styles.App}>
+        {/* styles wurde imported, App ist der Classname in App.module.css */}
+          <NavBar />
+          <Container className={styles.Main}>
+            <Switch>
+              <Route exact path="/" render={() => <h1>Home page</h1>} />
+              {/* wenn der route path matched, wird h1 gerendert */}
+              <Route exact path="/signin" render={() => <SignInForm />} />
+              <Route exact path="/signup" render={() => <SignUpForm />} />
+              <Route render={() => <p>Page not found!</p>} />
+            </Switch>
+          </Container>
+        </div>
+      </SetCurrentUserContext.Provider>
+    </CurrentUserContext.Provider>
   );
 }
 
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,36 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
 import logo from "../assets/logo.png";
 import styles from "../styles/NavBar.module.css";
 import { NavLink } from "react-router-dom";
+import { CurrentUserContext } from "../App";
 
 const NavBar = () => {
+  const currentUser = useContext(CurrentUserContext);
+
+  const loggedInIcons = <>{currentUser?.username}</>;
+  /* die müssen in leeren Elementen sein, damit das css nicht gestört wird */
+  const loggedOutIcons = (
+    <>
+      <NavLink
+        className={styles.NavLink}
+        activeClassName={styles.Active}
+        to="/signin"
+      >
+        <i className="fas fa-sign-in-alt"></i>Sign in
+      </NavLink>
+      <NavLink
+        to="/signup"
+        className={styles.NavLink}
+        activeClassName={styles.Active}
+      >
+        <i className="fas fa-user-plus"></i>Sign up
+      </NavLink>
+    </>
+  );
+
+  
+
   return (
     <Navbar className={styles.NavBar} expand="md" fixed="top">
       <Container>
@@ -13,6 +39,7 @@ const NavBar = () => {
             <img src={logo} alt="logo" height="45" />
           </Navbar.Brand>
         </NavLink>
+
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto text-left">
@@ -27,20 +54,9 @@ const NavBar = () => {
             >
               <i className="fas fa-home"></i>Home
             </NavLink>
-            <NavLink
-              className={styles.NavLink}
-              activeClassName={styles.Active}
-              to="/signin"
-            >
-              <i className="fas fa-sign-in-alt"></i>Sign in
-            </NavLink>
-            <NavLink
-              to="/signup"
-              className={styles.NavLink}
-              activeClassName={styles.Active}
-            >
-              <i className="fas fa-user-plus"></i>Sign up
-            </NavLink>
+
+            {currentUser ? loggedInIcons : loggedOutIcons}
+            {/* Icons je nach dem ob der User eingelogged ist oder nicht */}
           </Nav>
         </Navbar.Collapse>
       </Container>
