42db05c0e416ac5a85e8280cb459b6b6ec3b252d
danio86 <>
2023-05-30 10:55:13 +0000

    add api connection to signupform

--- src/pages/auth/SignUpForm.js
+++ src/pages/auth/SignUpForm.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+/* useState hook kann aus statischen, funktionale commponents bilden */
 import { Link, useHistory } from "react-router-dom";
 
 import styles from "../../styles/SignInUpForm.module.css";
@@ -22,26 +23,42 @@ const SignUpForm = () => {
     password1: "",
     password2: "",
   });
+  /* das sind die ursprungswerte für signUpData */
+  /* set SignUpData ist die Funktion, um diese Werte zu ändern*/
   const { username, password1, password2 } = signUpData;
+  /* hier werden die Werte gleich in ein dict gegeben (destructuriert) */
 
   const [errors, setErrors] = useState({});
+  /* der Hook wird aufgerufen, damit alle gespeicherten Errors angezeigt werden */
 
   const history = useHistory();
+  /* das ist ein fertiger hook der zurück navigiert */
+  /* der Import sollte oben automatisch funktionieren */
 
   const handleChange = (event) => {
     setSignUpData({
       ...signUpData,
       [event.target.name]: event.target.value,
+      /* das ist einfach ein key(name), value(value) paar */
+      /* key = input feld, value ist das, was der user eingibt */
     });
   };
+  /* ohne den handleChange können wir nichts in die Felder schreiben */
+  /* Zunächst müssen wir die vorherige "signUpData" verteilen,
+   damit wir nur das relevante Attribut aktualisieren müssen. 
+   Hierfür werden wir die Funktion der berechneten Eigenschaftsnamen in 
+   JavaScript verwenden und "event.target.name" auf "event.target.value" setzen. */
 
   const handleSubmit = async (event) => {
     event.preventDefault();
+    /* damit die Seite nicht refreshed */
     try {
       await axios.post("/dj-rest-auth/registration/", signUpData);
       history.push("/signin");
+      /* wir sollten dann zu signin navigiert werden */
     } catch (err) {
       setErrors(err.response?.data);
+      /* dieser error wird nach oben in den hook übergen */
     }
   };
 
@@ -67,6 +84,8 @@ const SignUpForm = () => {
               <Alert variant="warning" key={idx}>
                 {message}
               </Alert>
+              /* hier wird der error gerendert wenn es einen username error gibt */
+              /* die message ist automatisch */
             ))}
 
             <Form.Group controlId="password1">
@@ -81,7 +100,9 @@ const SignUpForm = () => {
                 placeholder="Password"
                 name="password1"
                 value={password1}
+                /* aus signupData */
                 onChange={handleChange}
+                /* der Handler wurde oben geschrieben */
               />
             </Form.Group>
             {errors.password1?.map((message, idx) => (
@@ -114,6 +135,7 @@ const SignUpForm = () => {
               Sign up
             </Button>
             {errors.non_field_errors?.map((message, idx) => (
+                /* non-field:error! */
               <Alert key={idx} variant="warning" className="mt-3">
                 {message}
               </Alert>
