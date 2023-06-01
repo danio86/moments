60f06d25995f99cfff886a2f311f365c20818756
danio86 <>
2023-06-01 12:18:56 +0000

    add post.js child

--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -7,6 +7,7 @@ import Container from "react-bootstrap/Container";
 import appStyles from "../../App.module.css";
 import { useParams } from "react-router";
 import { axiosReq } from "../../api/axiosDefaults";
+import Post from "./Post";
 
 function PostPage() {
   const { id } = useParams();
@@ -45,7 +46,14 @@ function PostPage() {
     <Row className="h-100">
       <Col className="py-2 p-0 p-lg-2" lg={8}>
         <p>Popular profiles for mobile</p>
-        <p>Post component</p>
+        <Post {...post.results[0]} setPosts={setPost} postPage />
+        {/* das ist das post.js-file */}
+        {/* we’ll spread the post object from the results array so that its key value
+        pairs are passed in as props */}
+        {/* We’ll also pass the setPost function in the setPosts prop, which we will use later
+        to handle our likes. */}
+        {/* postPage ist ein prop von/für der tochter (post.js) und ist true
+         */}
         <Container className={appStyles.Content}>Comments</Container>
       </Col>
       <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
