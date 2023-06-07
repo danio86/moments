d9d15189abbb70f821e6d2cb2de09e33bb8317bb
danio86 <>
2023-06-07 16:25:45 +0000

    removed Strict mode

--- src/index.js
+++ src/index.js
@@ -8,20 +8,44 @@ import { BrowserRouter as Router } from "react-router-dom";
 import { CurrentUserProvider } from "./contexts/CurrentUserContext";
 import { ProfileDataProvider } from "./contexts/ProfileDataContext";
 
+
+{/* React.StrictMode is a tool for highlighting
+    potential problems in an application by running additional checks and 
+    warnings on the application.
+    And wraps around all of our other contexts and components */}
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <Router>
+//     <CurrentUserProvider>
+//         <ProfileDataProvider>
+//           <App />
+//         </ProfileDataProvider>
+//       </CurrentUserProvider>
+//     </Router>
+//   </React.StrictMode>,
+//   document.getElementById("root")
+// );
+
 ReactDOM.render(
-  <React.StrictMode>
-    <Router>
-    <CurrentUserProvider>
-        <ProfileDataProvider>
-          <App />
-        </ProfileDataProvider>
-      </CurrentUserProvider>
-    </Router>
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+    
+      <Router>
+      <CurrentUserProvider>
+          <ProfileDataProvider>
+            <App />
+          </ProfileDataProvider>
+        </CurrentUserProvider>
+      </Router>,
+    document.getElementById("root")
+  );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+/* wenn wir uns ausloggen umd auf die konsole gehen sehen wir:
+Fehler, der uns mitteilt, dass findDOMNode in StrictMode veraltet ist. 
+Da StrictMode nur für Entwicklungszwecke gedacht ist und wir uns auf die endgültige 
+Bereitstellung vorbereiten, können wir es entfernen. */
+/* StrictMode kann einfach entfernt werden */
